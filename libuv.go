b883663b68a3398a35a55b035216f030477b7f19
xujintao <xujintao@126.com>
2019-07-21 14:38:03 +0800

    use uvlog

--- libuv.go
+++ libuv.go
@@ -42,13 +42,13 @@ func (pd *pollDesc) read() error {
 			switch err {
 			case syscall.EAGAIN:
 				if sum == 0 { // fd还没就绪
-					log.Println("fd not ready for reading")
+					uvlog.Print("fd not ready for reading")
 					pd.bufr = buf
 					return nil
 				}
 				done = true
 			default:
-				log.Println(err)
+				uvlog.Print(err)
 				return err
 			}
 		}
@@ -89,16 +89,16 @@ func (pd *pollDesc) write() error {
 		if n == -1 {
 			switch err {
 			case syscall.EAGAIN:
-				log.Println("fd not ready for writing")
+				uvlog.Print("fd not ready for writing")
 				pd.bufw = buf
 				return nil
 			default:
-				log.Println(err)
+				uvlog.Println(err)
 				return err
 			}
 		}
 
-		log.Printf("write bytes %d\n", n)
+		uvlog.Printf("write bytes %d", n)
 		buf = buf[n:] // 切
 		if 0 == len(buf) {
 			break
@@ -124,13 +124,13 @@ func (pd *pollDesc) Close() {
 	// 删除io监听
 	var event syscall.EpollEvent
 	if err := syscall.EpollCtl(epfd, syscall.EPOLL_CTL_DEL, pd.fd, &event); err != nil {
-		log.Println(err)
+		uvlog.Print(err)
 		return
 	}
 
 	// 主动关闭连接
 	if err := syscall.Close(pd.fd); err != nil {
-		log.Println(err)
+		uvlog.Print(err)
 		return
 	}
 }
@@ -160,14 +160,15 @@ func getAddr(sa syscall.Sockaddr) string {
 }
 
 var (
-	epfd int = -1
+	epfd  int = -1
+	uvlog     = log.New(os.Stdout, "[UVLOG] ", log.LstdFlags|log.Lshortfile)
 )
 
 func init() {
 	var err error
 	epfd, err = syscall.EpollCreate1(syscall.EPOLL_CLOEXEC)
 	if err != nil {
-		log.Println(err)
+		uvlog.Println(err)
 		os.Exit(1)
 	}
 }
@@ -178,19 +179,19 @@ func Wait() {
 		// msec参数runtime那边是0，非阻塞。这里设置为-1，阻塞
 		n, err := syscall.EpollWait(epfd, events[:], -1)
 		if err != nil {
-			log.Println(err)
+			uvlog.Println(err)
 			break
 		}
 
 		for i := 0; i < n; i++ {
 			e := events[i]
 			if e.Events == 0 {
-				log.Println("wait continue")
+				uvlog.Println("wait continue")
 				continue
 			}
 
 			if e.Events&(syscall.EPOLLERR|syscall.EPOLLHUP) != 0 {
-				log.Println(e.Events)
+				uvlog.Println(e.Events)
 				pd := *(**pollDesc)(unsafe.Pointer(&e.Fd))
 				pd.Close()
 				continue
@@ -200,14 +201,20 @@ func Wait() {
 			case e.Events&syscall.EPOLLIN != 0:
 				pd := *(**pollDesc)(unsafe.Pointer(&e.Fd))
 				if pd.islistener {
+					uvlog.Println("accept event active------>")
 					accept(epfd, e)
+					uvlog.Println("------>accept event deprecated")
 					break
 				}
+				uvlog.Println("read event active------>")
 				read(epfd, e)
+				uvlog.Println("------>read event deprecated")
 			case e.Events&syscall.EPOLLOUT != 0:
+				uvlog.Println("write event active------>")
 				write(epfd, e)
+				uvlog.Println("------>write event deprecated")
 			default:
-				fmt.Printf("poll continue, event(%v)", e)
+				uvlog.Printf("poll continue, event(%v)", e)
 			}
 		}
 	}
@@ -217,30 +224,29 @@ func Wait() {
 func Start(address string, handler Handler) {
 	listenfd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM|syscall.SOCK_NONBLOCK|syscall.SOCK_CLOEXEC, 0)
 	if err != nil {
-		log.Println(err)
+		uvlog.Println(err)
 		os.Exit(1)
 	}
 
 	// 只有listenfd需要设置
 	if err := syscall.SetsockoptInt(listenfd, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1); err != nil {
-		log.Println(err)
+		uvlog.Println(err)
 		os.Exit(1)
 	}
 
 	addr := syscall.SockaddrInet4{Port: 8080}
 	copy(addr.Addr[:], net.ParseIP("0.0.0.0").To4())
 	if err := syscall.Bind(listenfd, &addr); err != nil {
-		log.Println(err)
+		uvlog.Println(err)
 		os.Exit(1)
 	}
 
 	if err := syscall.Listen(listenfd, 128); err != nil {
-		log.Println(err)
+		uvlog.Println(err)
 		os.Exit(1)
 	}
 
-	lsa, _ := syscall.Getsockname(listenfd)
-	log.Println(lsa)
+	uvlog.Println("uv start")
 
 	pd := pollDesc{
 		listenfd:   listenfd,
@@ -252,7 +258,7 @@ func Start(address string, handler Handler) {
 	*(**pollDesc)(unsafe.Pointer(&event.Fd)) = &pd
 	event.Events = syscall.EPOLLIN | syscall.EPOLLOUT | syscall.EPOLLRDHUP | (-syscall.EPOLLET)
 	if err := syscall.EpollCtl(epfd, syscall.EPOLL_CTL_ADD, listenfd, &event); err != nil {
-		log.Println(err)
+		uvlog.Println(err)
 		os.Exit(1)
 	}
 }
@@ -260,17 +266,20 @@ func Start(address string, handler Handler) {
 func accept(epfd int, e syscall.EpollEvent) {
 	lpd := *(**pollDesc)(unsafe.Pointer(&e.Fd))
 	listenfd := lpd.listenfd
+
+	cnt := 0
 	for {
 		connfd, sa, err := syscall.Accept4(listenfd, syscall.SOCK_NONBLOCK|syscall.SOCK_CLOEXEC)
 		if connfd == -1 {
 			switch err {
 			case syscall.EAGAIN:
-				log.Println("we have processed all incoming conn")
+				uvlog.Printf("we have processed %d incoming conn", cnt)
 			default:
-				log.Println(err)
+				uvlog.Print(err)
 			}
 			return
 		}
+		cnt++
 
 		// v, err := syscall.GetsockoptInt(connfd, syscall.SOL_SOCKET, syscall.SO_SNDBUF)
 		// if err != nil {
@@ -299,7 +308,7 @@ func accept(epfd int, e syscall.EpollEvent) {
 		*(**pollDesc)(unsafe.Pointer(&event.Fd)) = &cpd
 		event.Events = syscall.EPOLLIN | syscall.EPOLLOUT | syscall.EPOLLRDHUP | (-syscall.EPOLLET)
 		if err := syscall.EpollCtl(epfd, syscall.EPOLL_CTL_ADD, connfd, &event); err != nil {
-			log.Println(err)
+			uvlog.Print(err)
 			os.Exit(1)
 		}
 
@@ -314,7 +323,7 @@ func read(epfd int, e syscall.EpollEvent) error {
 	// 这里有问题，如果连接来了，用户协议是先写那么就没有配读缓存及读回调
 	// 然后就会丢失客户端的关闭连接
 	if len(pd.bufr) == 0 {
-		log.Println("empty buf")
+		uvlog.Print("empty buf")
 		return nil
 	}
 	return pd.read()
@@ -323,7 +332,7 @@ func read(epfd int, e syscall.EpollEvent) error {
 func write(epfd int, e syscall.EpollEvent) error {
 	pd := *(**pollDesc)(unsafe.Pointer(&e.Fd))
 	if len(pd.bufw) == 0 {
-		log.Println("nothing to write")
+		uvlog.Print("nothing to write")
 		return nil
 	}
 	return pd.write()
